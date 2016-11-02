ab5472279be758c835dacc64559982940e32e937
michaelplott <mike.plott80@gmail.com>
2016-11-02 11:36:51 -0400

    tic tac toe set up

--- src/clojure_tic_tac_toe/core.clj
+++ src/clojure_tic_tac_toe/core.clj
@@ -1,6 +1,58 @@
 (ns clojure-tic-tac-toe.core
   (:gen-class))
 
+(def board [[:x :e :o]
+            [:x :o :e]
+            [:x :e :o]])
+;literal
+[1 2 3] 
+{:name "Mike"}
+'(1 2 3)
+#{1 2 3}
+
+;non literal
+(vector 1 2 3)
+(hash-map :name "Mike")
+(list 1 2 3)
+(hash-set 1 2 3)
+
 (defn -main [& args]
-  (println "Hello, world!"))
+  (let [[[a b c]
+         [d e f]
+         [g h i]] board
+        solutions (hash-set
+                    [a b c]
+                    [d e f]
+                    [g h i]
+                    [a d g]
+                    [b e h]
+                    [c f i]
+                    [a e i]
+                    [g e c])]
+    (cond
+      (contains? solutions [:x :x :x])
+      (println "X has won!")
+      (contains? solutions [:o :o :o])
+      (println "O has won!")
+      :else
+      (println "Nobody won!"))))
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
